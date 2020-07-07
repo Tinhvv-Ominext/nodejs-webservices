15af3f2895ac4bb1fc0a80078028c38d6f39fe87
TinhVV <tinhvv@ominext.com>
2020-07-07 17:29:17 +0700

    welcome

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "tinhvv-secret-key"
+  };
---
+++ app/controllers/auth.controller.js
@@ -0,0 +1,117 @@
+const config = require("../config/auth.config");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
+function getStandardResponse(status, message, data){
+    return {
+        status: status,
+        message : message,
+        data : data
+     }
+}
+
+exports.signup = (req, res) => {
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8)
+    });
+  
+    user.save((err, user) => {
+      if (err) {
+        res.status(500).send(getStandardResponse(0, err, null));
+        return;
+      }
+  
+      if (req.body.roles) {
+        Role.find(
+          {
+            name: { $in: req.body.roles }
+          },
+          (err, roles) => {
+            if (err) {
+              res.status(500).send(getStandardResponse(0, err, null));
+              return;
+            }
+  
+            user.roles = roles.map(role => role._id);
+            user.save(err => {
+              if (err) {
+                res.status(500).send(getStandardResponse(0, err, null));
+                return;
+              }
+  
+              res.send(getStandardResponse(1, "User was registered successfully!", null));
+            });
+          }
+        );
+      } else {
+        Role.findOne({ name: "user" }, (err, role) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+  
+          user.roles = [role._id];
+          user.save(err => {
+            if (err) {
+              res.status(500).send({ message: err });
+              return;
+            }
+  
+            res.send(getStandardResponse(1, "User was registered successfully!", null));
+          });
+        });
+      }
+    });
+  };
+  
+  exports.signin = (req, res) => {
+    User.findOne({
+      username: req.body.username
+    })
+      .populate("roles", "-__v")
+      .exec((err, user) => {
+        if (err) {
+          res.status(500).send(getStandardResponse(0, err, null));
+          return;
+        }
+  
+        if (!user) {
+          return res.status(404).send(getStandardResponse(0, "User not found!", null));
+        }
+  
+        var passwordIsValid = bcrypt.compareSync(
+          req.body.password,
+          user.password
+        );
+  
+        if (!passwordIsValid) {
+          return res.status(401).send(getStandardResponse(0, "Password invalid", null));
+        }
+  
+        var token = jwt.sign({ id: user.id }, config.secret, {
+          expiresIn: 86400 // 24 hours
+        });
+  
+        var authorities = [];
+  
+        for (let i = 0; i < user.roles.length; i++) {
+          authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+        }
+
+        let data = {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            roles: authorities,
+            accessToken: token
+          };
+
+        res.status(200).send(getStandardResponse(1, "Success", data));
+      });
+  };
